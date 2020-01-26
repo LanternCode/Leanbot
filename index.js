3f0b660c3590eb25906b49ec3e90eb9571f6a12c
LanternCode <Adam.Machowczyk@gmail.com>
2020-01-26 15:51:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+﻿const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const token = 'discordAppToken';
+
+let phase = 1;
+let iteration = 0;
+let iterationCount = 0;
+
+let signsEnum = {
+    TOP : '↑',
+    BOTTOM : '↓',
+    RIGHT : '→',
+	LEFT : '←'
+};
+
+let loopInterval;
+
+bot.on('ready', () => {
+console.log('Am online.');
+});
+
+bot.on('message', msg => {
+	if(msg.content === "!start"){
+		msg.reply("Sequence activated.");
+	}
+	else if(msg.content === "!testmsgrelay"){
+		msg.reply("Answer1");
+		msg.reply("Answer2");
+		msg.reply("Answer3");
+		msg.reply("Answer4");
+		msg.reply("Answer5");
+	}else if(msg.content === "!speedrun"){
+		loopInterval = setInterval(() => {
+			msg.channel.send(iterationCount == 4 ? signsEnum.TOP : '.');
+			iterationCount = iterationCount == 4 ? 0 : iterationCount+1;
+		}, 3000);
+	}else if(msg.content === "!pause"){
+		clearInterval(loopInterval);
+	}
+});
+
+bot.login(token);
