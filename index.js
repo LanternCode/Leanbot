9995b4e78874bb1d5cdb3a3c0ee0a545b9fae4df
LanternCode <Adam.Machowczyk@gmail.com>
2020-02-16 02:02:29 +0000

    Finalized the intended functionality

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 ﻿const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'discordAppToken';
-
-let phase = 1;
-let iteration = 0;
-let iterationCount = 0;
+const token = '';
 
 let signsEnum = {
     TOP : '↑',
@@ -14,6 +10,22 @@ let signsEnum = {
 	LEFT : '←'
 };
 
+let phases = [
+        [
+            signsEnum.LEFT, signsEnum.TOP, signsEnum.LEFT, signsEnum.RIGHT, signsEnum.TOP
+        ],
+        [
+            signsEnum.LEFT, signsEnum.LEFT, signsEnum.RIGHT, signsEnum.LEFT, signsEnum.RIGHT
+        ],
+        [
+            signsEnum.RIGHT, signsEnum.TOP, signsEnum.RIGHT, signsEnum.TOP, signsEnum.TOP
+        ]
+];
+
+let phaseNumber = 0;
+let phaseIterator = 0;
+let iterationCount = 0;
+
 let loopInterval;
 
 bot.on('ready', () => {
@@ -21,23 +33,33 @@ console.log('Am online.');
 });
 
 bot.on('message', msg => {
-	if(msg.content === "!start"){
-		msg.reply("Sequence activated.");
-	}
-	else if(msg.content === "!testmsgrelay"){
-		msg.reply("Answer1");
-		msg.reply("Answer2");
-		msg.reply("Answer3");
-		msg.reply("Answer4");
-		msg.reply("Answer5");
-	}else if(msg.content === "!speedrun"){
+	if(msg.content === "!leanbot start"){
+        msg.channel.send("Leanbot started!");
 		loopInterval = setInterval(() => {
-			msg.channel.send(iterationCount == 4 ? signsEnum.TOP : '.');
-			iterationCount = iterationCount == 4 ? 0 : iterationCount+1;
+			msg.channel.send(iterationCount == 4 ? phases[phaseNumber][phaseIterator] : '.'); //Send a '.' or an arrow
+
+            phaseIterator = (iterationCount == 4) ? ((phaseIterator == 4) ? 0 : phaseIterator+1) : phaseIterator; //Which part of the phase are we in
+			iterationCount = iterationCount == 4 ? 0 : iterationCount+1; //Are we to display a '.' or an arrow next time
 		}, 3000);
-	}else if(msg.content === "!pause"){
+	}else if(msg.content === "!leanbot pause"){ //remporarily pause the loop
 		clearInterval(loopInterval);
-	}
+        msg.channel.send("Leanbot paused.");
+	}else if(msg.content === "!leanbot go"){ //increase phase by one
+        if(phaseNumber == 2){
+            msg.channel.send("Final sequence reached. Use '!leanbot stop' to reset all values.");
+        }else{
+            phaseNumber++;
+            phaseIterator = 0; //start with a new, clear phase
+            iterationCount = 0;
+            msg.channel.send("Sequence escalated.");
+        }
+    }else if(msg.content === "!leanbot stop"){
+        clearInterval(loopInterval); //reset the loop
+        phaseNumber = 0; //clear all values managed by the loop
+        phaseIterator = 0;
+        iterationCount = 0;
+        msg.channel.send("All values were reset.");
+    }
 });
 
 bot.login(token);
